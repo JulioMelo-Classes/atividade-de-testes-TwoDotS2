593291da079c15e1616b4741dff3123ac6ece91b
juliocpmelo <julio.melo@imd.ufrn.br>
2022-03-31 17:49:06 -0300

    teste commit no terminal

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -3,8 +3,8 @@
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     cout<<n<<endl;
-    // TODO: adicione o seu código aqui.
-
+    // TODO: adicione o seu código aqui.    
+    cout<<"teste 2"<<endl;
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
     return std::vector<unsigned int>{};
 }
